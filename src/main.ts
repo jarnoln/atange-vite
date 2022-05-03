784639f27eb9cbd323313405fb4be554a32e1617
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2022-05-03 16:35:15 +0300

    Add CollectiveView

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import CollectiveList from './components/CollectiveList.vue'
 import HelloWorld from './components/HelloWorld.vue'
+import CollectiveView from './views/CollectiveView.vue'
 
 const router = createRouter({
     history: createWebHistory(), // Use browser built-in history
     routes: [
-        { path: '/', component: CollectiveList },
-        { path: '/hello', component: HelloWorld },
+        { path: '/', name: 'home', component: CollectiveList },
+        { path: '/login', name: 'login', component: HelloWorld },
+        { path: '/c/:collectiveName', name: 'collective', component: CollectiveView }
     ]
 })
 
---
+++ src/views/CollectiveView.vue
@@ -0,0 +1,27 @@
+<template>
+  <table v-if="collectiveStore.selectedCollective !== undefined">
+    <tbody>
+      <tr>
+        <th> Title </th>
+        <td> {{ collectiveStore.selectedCollective.title }}</td>
+      </tr>
+    </tbody>
+  </table>
+  <p v-else>Unknown collective</p>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { useCollectiveStore } from '../stores/CollectiveStore'
+
+const collectiveStore = useCollectiveStore()
+collectiveStore.addExampleCollectives()
+const route = useRoute()
+
+onMounted(() => {
+    const collectiveName = route.params.collectiveName as string
+    collectiveStore.selectCollective(collectiveName)
+})
+
+</script>
