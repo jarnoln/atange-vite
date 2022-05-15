a4fb03c1a47effe3321fbd78290c3bee1dcb3167
Jarno Luoma-Nirva <jln@iki.fi>
2022-05-15 14:30:44 +0800

    Own component for NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,5 @@
 <template>
-  <nav id="navbar">
-    <ul classid="right-links">
-      <li>
-        <router-link active-class="active" to="/">Home</router-link>
-      </li>
-    </ul>
-    <ul id="left-links">
-      <li v-if="!isLoggedIn()">
-        <router-link active-class="active" to="/login">Login</router-link>
-      </li>
-      <li v-if="!isLoggedIn()">
-        <router-link active-class="active" to="/register">Register</router-link>
-      </li>
-      <li v-if="isLoggedIn()">
-        <a href="#">{{ getUsername() }}</a>
-      </li>
-      <li v-if="isLoggedIn()">
-        <a href="#" @click="logout()">Logout</a>
-      </li>
-    </ul>
-  </nav>
+  <nav-bar />
   <notifications />
   <div class="container">
     <router-view></router-view>
@@ -27,58 +7,11 @@
 </template>
 
 <script setup lang="ts">
-import { useSessionStore } from './stores/SessionStore'
-import { serverLogout } from './services/EventService'
+import NavBar from './components/NavBar.vue'
 import Notifications from './components/Notifications.vue'
-
-const sessionStore = useSessionStore()
-
-function isLoggedIn() {
-  return sessionStore.isLoggedIn
-}
-
-function getUsername() {
-  return sessionStore.username
-}
-
-function logout() {
-  serverLogout()
-  sessionStore.logout()
-}
 </script>
 
 <style scoped>
-#navbar {
-  width: 100%;
-  background-color: navy;
-  display: flex;
-  justify-content: space-between;
-}
-
-ul {
-  list-style: none;
-  display: flex;
-  padding-left: 1rem;
-}
-
-#left-links {
-  padding-right: 2rem;
-}
-
-a {
-  text-decoration: none;
-  color: grey;
-  padding: 1rem;
-}
-
-a:hover {
-  background-color: rgba(255, 255, 255, 0.1);
-}
-
-a.active {
-  color: white;
-}
-
 .container {
   margin-left: 4rem;
   margin-right: 4rem;
