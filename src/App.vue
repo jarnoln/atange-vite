c827df713cc87afe138023803e0800670b3e4d61
Jarno Luoma-Nirva <jln@iki.fi>
2022-05-15 13:50:05 +0800

    Own component for notifications

--- src/App.vue
+++ src/App.vue
@@ -20,22 +20,18 @@
       </li>
     </ul>
   </nav>
-  <div id="notifications" :class="getNotificationClass()">
-    {{ getNotificationMessage() }}
-  </div>
+  <notifications />
   <div class="container">
     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useSessionStore } from './stores/SessionStore';
-import { useNotificationStore } from './stores/NotificationStore';
-import { serverLogout } from './services/EventService';
+import { useSessionStore } from './stores/SessionStore'
+import { serverLogout } from './services/EventService'
+import Notifications from './components/Notifications.vue'
 
 const sessionStore = useSessionStore()
-const notificationStore = useNotificationStore()
 
 function isLoggedIn() {
   return sessionStore.isLoggedIn
@@ -48,22 +44,6 @@ function getUsername() {
 function logout() {
   serverLogout()
   sessionStore.logout()
-}
-
-function getNotificationMessage() {
-  if (notificationStore.notifications.length === 0) {
-    return 'Placeholder'
-  } else {
-    return notificationStore.notifications[0].message
-  }
-}
-
-function getNotificationClass() {
-  if (notificationStore.notifications.length === 0) {
-    return 'invisible'
-  } else {
-    return notificationStore.notifications[0].class
-  }
 }
 </script>
 
@@ -102,28 +82,5 @@ a.active {
 .container {
   margin-left: 4rem;
   margin-right: 4rem;
-}
-
-#notifications {
-  padding: 5px;
-}
-
-#notifications.info {
-  color: #000033;
-  background-color: #bbbbff;
-}
-#notifications.success {
-  color: #003300;
-  background-color: #bbffbb;
-}
-
-#notifications.error {
-  color: #330000;
-  background-color: #f55;
-}
-
-#notifications.invisible {
-  color: rgba(0, 0, 0, 0);
-  background-color:rgba(0, 0, 0, 0);
 }
 </style>
---
+++ src/components/Notifications.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="notifications" :class="getNotificationClass()">
+    {{ getNotificationMessage() }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useNotificationStore } from '../stores/NotificationStore';
+
+const notificationStore = useNotificationStore()
+
+function getNotificationMessage() {
+  if (notificationStore.notifications.length === 0) {
+    return 'Placeholder'
+  } else {
+    return notificationStore.notifications[0].message
+  }
+}
+
+function getNotificationClass() {
+  if (notificationStore.notifications.length === 0) {
+    return 'invisible'
+  } else {
+    return notificationStore.notifications[0].class
+  }
+}
+</script>
+
+<style scoped>
+#notifications {
+  padding: 5px;
+}
+
+#notifications.info {
+  color: #000033;
+  background-color: #bbbbff;
+}
+#notifications.success {
+  color: #003300;
+  background-color: #bbffbb;
+}
+
+#notifications.error {
+  color: #330000;
+  background-color: #f55;
+}
+
+#notifications.invisible {
+  color: rgba(0, 0, 0, 0);
+  background-color:rgba(0, 0, 0, 0);
+}
+</style>
