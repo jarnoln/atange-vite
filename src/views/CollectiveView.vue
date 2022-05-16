9bc8aaf9ec75a7b5147e87d3363aa68647c5564e
Jarno Luoma-Nirva <jln@iki.fi>
2022-05-16 09:24:39 +0800

    Add delete button to collective page

--- src/views/CollectiveView.vue
+++ src/views/CollectiveView.vue
@@ -1,14 +1,17 @@
 <template>
   <div v-if="collectiveStore.selectedCollective !== undefined">
     <h1 id="collective-title"> {{ collectiveStore.selectedCollective.title }} </h1>
+    <button @click="deleteSelectedCollective()">Delete collective</button>
   </div>
   <p v-else>Unknown collective</p>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 // import { useRoute } from 'vue-router'
+import { Collective } from '../types'
 import { useCollectiveStore } from '../stores/CollectiveStore'
+import { EventService } from '../services/EventService';
 
 const props = defineProps<{
   collectiveName: string
@@ -19,8 +22,15 @@ collectiveStore.addExampleCollectives()
 // const route = useRoute()
 
 onMounted(() => {
-    // const collectiveName = route.params.collectiveName as string
-    collectiveStore.selectCollective(props.collectiveName)
+  // const collectiveName = route.params.collectiveName as string
+  collectiveStore.selectCollective(props.collectiveName)
 })
 
+function deleteSelectedCollective() {
+  const collective = collectiveStore.selectedCollective
+  if (collective) {
+    collectiveStore.deleteCollective(collective)
+    EventService.deleteCollective(collective)
+  }
+}
 </script>
