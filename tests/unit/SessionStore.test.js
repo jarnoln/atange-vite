bffe31c381db7c2007e193a8969d63f32ce505f9
Jarno Luoma-Nirva <jln@iki.fi>
2022-05-19 11:49:59 +0800

    Add unit tests for SessionStore

---
+++ tests/unit/SessionStore.test.js
@@ -0,0 +1,35 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { beforeEach, describe, expect, it, vi, vitest } from 'vitest'
+import { useSessionStore } from '../../src/stores/SessionStore'
+
+vi.mock('axios')
+
+const pinia = createPinia()
+setActivePinia(pinia)
+
+const sessionStore = useSessionStore()
+
+describe('Test SessionStore', () => {
+  beforeEach(() => {
+    sessionStore.clear()
+  }),
+  it('can login', () => {
+    expect(sessionStore.username).toBe('')
+    expect(sessionStore.token).toBe('')
+    expect(sessionStore.isLoggedIn).toBe(false)
+    sessionStore.login('superman', 'abcd')
+    expect(sessionStore.username).toBe('superman')
+    expect(sessionStore.token).toBe('abcd')
+    expect(sessionStore.isLoggedIn).toBe(true)
+  }),
+  it('can logout', () => {
+    sessionStore.login('superman', 'abcd')
+    expect(sessionStore.username).toBe('superman')
+    expect(sessionStore.token).toBe('abcd')
+    expect(sessionStore.isLoggedIn).toBe(true)
+    sessionStore.logout()
+    expect(sessionStore.username).toBe('')
+    expect(sessionStore.token).toBe('')
+    expect(sessionStore.isLoggedIn).toBe(false)
+  })
+})
