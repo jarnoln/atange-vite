ccf15df9de4ee84f675e1e0ed372526319a5045a
Jarno Luoma-Nirva <jln@iki.fi>
2022-10-23 20:44:23 +0300

    Can vote from question list page

--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -9,6 +9,7 @@
       <thead>
         <tr>
           <th class="text-left dim-background" style="width:90%">Question</th>
+          <th colspan="3" class="text-left dim-background" v-if="sessionStore.isLoggedIn">Actions</th>
           <th class="text-right dim-background">Yes</th>
           <th class="text-right dim-background">Abs</th>
           <th class="text-right dim-background">No</th>
@@ -18,16 +19,25 @@
       </thead>
       <tbody>
         <template v-for="question in questionStore.questions" :key="question.name">
-          <tr v-if="question.itemType === 'Q'">
+          <tr v-if="question.itemType === 'Q' || question.itemType === 'question'">
             <td>
               <router-link :to="{ name: 'question', params: { collectiveName: collectiveStore.currentCollective.name, questionName: question.name }}">
                 {{ question.title }}
               </router-link>
             </td>
+            <td class="text-right" v-if="sessionStore.isLoggedIn">
+              <button class="btn-small" id="answer-yes-btn" @click="voteYes(question.name)">Yes</button>
+            </td>
+            <td class="text-right" v-if="sessionStore.isLoggedIn">
+              <button class="btn-small" id="answer-abstain-btn" @click="voteAbstain(question.name)">Abstain</button>
+            </td>
+            <td class="text-right" v-if="sessionStore.isLoggedIn">
+              <button class="btn-small" id="answer-no-btn" @click="voteNo(question.name)">No</button>
+            </td>
             <td class="text-right">{{ questionStore.getApproval(question.name).yes }}</td>
             <td class="text-right">{{ questionStore.getApproval(question.name).abstain }}</td>
             <td class="text-right">{{ questionStore.getApproval(question.name).no }}</td>
-            <td class="text-right"> {{ questionStore.getApproval(question.name).approvalPct }} %</td>
+            <td class="text-right"> {{ questionStore.getApproval(question.name).approvalPct }}%</td>
             <td v-if="canEditQuestions">
               <router-link
                 :to="{ name: 'question-edit', params: { collectiveName: collectiveStore.currentCollective.name, questionName: question.name }}">
@@ -36,7 +46,7 @@
             </td>
           </tr>
           <tr v-else>
-            <td class="light-background" colspan="5"> {{ question.title }}</td>
+            <td class="light-background" :colspan="sessionStore.isLoggedIn ? 8 : 5"> {{ question.title }}</td>
             <td class="light-background" v-if="canEditQuestions">
               <router-link
                 :to="{ name: 'question-edit', params: { collectiveName: collectiveStore.currentCollective.name, questionName: question.name }}">
@@ -69,6 +79,7 @@ import { useCollectiveStore } from '../stores/CollectiveStore'
 import { useNotificationStore } from '../stores/NotificationStore'
 import { useQuestionStore } from '../stores/QuestionStore'
 import { useSessionStore } from '../stores/SessionStore'
+import { EventService } from '../services/EventService'
 
 const collectiveStore = useCollectiveStore()
 const questionStore = useQuestionStore()
@@ -83,6 +94,24 @@ const canEditQuestions = computed(() => {
   }
   return false
 })
+
+function updateAnswer(questionName: string, vote: number) {
+  questionStore.setAnswer(questionName, sessionStore.username, vote, '')
+  EventService.updateAnswer(questionName, sessionStore.username, vote, '')
+}
+
+function voteYes(questionName: string) {
+  updateAnswer(questionName, 1)
+}
+
+function voteAbstain(questionName: string) {
+  updateAnswer(questionName, 0)
+}
+
+function voteNo(questionName: string) {
+  updateAnswer(questionName, -1)
+}
+
 </script>
 
 <style scoped>
@@ -108,4 +137,22 @@ caption {
   font-weight: bold;
   /* caption-side: bottom; */
 }
+
+.btn-small {
+  font: inherit;
+  text-decoration: none;
+  border: 2px solid #512483;
+  color: white;
+  background-color: #512483;
+  padding: 0.1rem 0.2rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 0 0.5rem 0 0;
+}
+
+.btn-small:hover {
+  box-shadow: none;
+  border: 2px solid black;
+  background-color: #613493;
+}
+
 </style>
